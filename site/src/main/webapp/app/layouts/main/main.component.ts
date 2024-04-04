6c0d5e1263727b65c59e2c24703da753f8cdadde
VmrSothis <victor.maree@sothis.tech>
2024-04-04 12:49:36 +0200

    updated scss , updated main

--- site/src/main/webapp/app/layouts/main/main.component.ts
+++ site/src/main/webapp/app/layouts/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, RendererFactory2, Renderer2 } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
+import { RouterOutlet, Router, ActivatedRoute } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import dayjs from 'dayjs/esm';
 
@@ -9,18 +9,24 @@ import FooterComponent from '../footer/footer.component';
 import PageRibbonComponent from '../profiles/page-ribbon.component';
 import { SideNavbarComponent } from '../side-navbar/side-navbar.component';
 import { BreadcrumbBarComponent } from '../breadcrumb-bar/breadcrumb-bar.component';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ModalInfoComponent } from '../modals/modal-info/modal-info.component';
+
 
 @Component({
   selector: 'jhi-main',
   standalone: true,
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent, SideNavbarComponent, BreadcrumbBarComponent],
+  imports: [RouterOutlet, FooterComponent, PageRibbonComponent, SideNavbarComponent, BreadcrumbBarComponent, CommonModule, MatDialogModule ],
 })
 export default class MainComponent implements OnInit {
+  isMainPage: boolean = false;
   private renderer: Renderer2;
 
   constructor(
+    public dialog: MatDialog,
     private router: Router,
     private appPageTitleStrategy: AppPageTitleStrategy,
     private accountService: AccountService,
@@ -28,16 +34,28 @@ export default class MainComponent implements OnInit {
     rootRenderer: RendererFactory2,
   ) {
     this.renderer = rootRenderer.createRenderer(document.querySelector('html'), null);
+    
   }
 
   ngOnInit(): void {
     // try to log in automatically
     this.accountService.identity().subscribe();
+    this.isMainPage = (this.router.url === '/');
+
+    console.error(this.router.url);
 
     this.translateService.onLangChange.subscribe((langChangeEvent: LangChangeEvent) => {
       this.appPageTitleStrategy.updateTitle(this.router.routerState.snapshot);
       dayjs.locale(langChangeEvent.lang);
       this.renderer.setAttribute(document.querySelector('html'), 'lang', langChangeEvent.lang);
+    });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ModalInfoComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.error(`Dialog result: ${result}`);
     });
   }
 }

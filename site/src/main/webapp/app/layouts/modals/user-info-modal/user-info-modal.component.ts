6a6d6c828e2f073bc6b2f7ecd7d9fe5f7b3c63ee
VmrSothis <victor.maree@sothis.tech>
2024-04-04 16:53:26 +0200

    create user-info-modal

---
+++ site/src/main/webapp/app/layouts/modals/user-info-modal/user-info-modal.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { User } from 'app/admin/user-management/user-management.model';
+
+@Component({
+  selector: 'user-info-modal',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './user-info-modal.component.html',
+  styleUrl: './user-info-modal.component.scss'
+})
+export class UserInfoModalComponent {
+  userSelected: User | null = null;
+  modalType: string = 'view';
+
+  constructor(private dialogRef: MatDialogRef<User>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.modalType = this.data.modalType ;
+  }
+  
+  confirm(): void {
+    this.dialogRef.close({ option: 'confirm', user: this.data.userdata});
+  }
+
+}

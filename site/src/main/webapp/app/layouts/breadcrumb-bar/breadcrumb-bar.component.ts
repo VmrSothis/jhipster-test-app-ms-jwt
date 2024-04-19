a3f5d763f332bf5cafe5f913d0c67e65b260c212
JGLSothis <jacob.garcia@sothis.tech>
2024-04-19 12:34:06 +0200

    Modificación breadcrumb - JGL
    Se ha añadido redireccionar al hacer clic en un segmento del breadcrumb
    Se ha omitido el segmento admin

--- site/src/main/webapp/app/layouts/breadcrumb-bar/breadcrumb-bar.component.ts
+++ site/src/main/webapp/app/layouts/breadcrumb-bar/breadcrumb-bar.component.ts
@@ -32,19 +32,17 @@ export class BreadcrumbBarComponent implements OnInit {
     this.breadcrumbItems = this.generateBreadcrumbs(urlSegments); // Genera las migas de pan
   }
 
-  // Genera las migas de pan basadas en los segmentos de la URL
   private generateBreadcrumbs(urlSegments: string[]): { label: string, url: string }[] {
     const breadcrumbs: { label: string, url: string }[] = [];
 
-    breadcrumbs.push({ label: 'breadcrumb.home', url: '' }); // Agrega "Inicio" como primera miga de pan
+    breadcrumbs.push({ label: 'breadcrumb.home', url: '' });
 
     for (let i = 0; i < urlSegments.length; i++) {
-      const currentUrl = `/${urlSegments.slice(0, i + 1).join('/')}`; // Construye la URL acumulativa hasta el segmento actual
-      const matchingBreadcrumb = this.breadcrumbItems.find(item => item.url === currentUrl); // Busca si la URL acumulativa ya existe en las migas de pan
-      if (matchingBreadcrumb) { // Si la URL acumulativa existe en las migas de pan
-        breadcrumbs.push(matchingBreadcrumb); // Agrega la miga de pan correspondiente
-      } else { // Si la URL acumulativa no existe en las migas de pan
-        breadcrumbs.push({ label: 'breadcrumb.' + urlSegments[i], url: currentUrl }); // Agrega el segmento de la URL como nueva miga de pan
+      const currentUrl = `/${urlSegments.slice(0, i + 1).join('/')}`;
+      const matchingBreadcrumb = this.breadcrumbItems.find(item => item.url === currentUrl);
+      // Mantener la lógica anterior y agregar la condición para omitir "admin"
+      if (!matchingBreadcrumb && urlSegments[i] !== 'admin') {
+        breadcrumbs.push({ label: 'breadcrumb.' + urlSegments[i], url: currentUrl });
       }
     }
     return breadcrumbs;
@@ -53,5 +51,10 @@ export class BreadcrumbBarComponent implements OnInit {
   // Retrocede en la historia del navegador
   goBack(): void {
     this.location.back();
+  }
+
+  // Método para redireccionar a la URL especificada
+  navigateTo(url: string): void {
+    this.router.navigateByUrl(url);
   }
 }
